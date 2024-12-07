e6a5009088eb811dbcafacc9c314aa712504c9d5
Dumitru Bianca Stefania <57909399+dumitrustefania@users.noreply.github.com>
2024-12-07 23:21:35 +0200

    Create rfid.h

---
+++ web_app/uC_code/rfid.h
@@ -0,0 +1,38 @@
+#include <MFRC522.h>
+
+void initializeRFID(MFRC522 &rfid) {
+  SPI.begin();
+  rfid.PCD_Init();
+}
+
+bool getCardID(MFRC522 rfid, String &cardID) {
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  Serial.print("PICC type: ");
+  Serial.println(rfid.PICC_GetTypeName(piccType));
+
+  // Check if the PICC is of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println("Your tag is not of type MIFARE Classic.");
+    return false;
+  }
+
+  // Read RFID card ID
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    cardID += String(rfid.uid.uidByte[i] < 0x10 ? "0" : "");
+    cardID += String(rfid.uid.uidByte[i], HEX);
+  }
+  Serial.print("Card ID is ");
+  Serial.println(cardID);
+
+  return true;
+}
+
+void stopRFID(MFRC522 &rfid) {
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+}
